85d9dcd32b150d87a9c2bebd33e181ce8a01b3b4
sadia-mohamed <sacduushm@gmail.com>
2022-07-18 20:03:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const cards = document.querySelectorAll(".card");
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+  }
+}
+
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 1) {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000);
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    },1200);
+}
+
+
+function shuffleCard() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        imgTag.src = `images/img-${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+shuffleCard();
+    
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
+});
+
+
+
+function mybutton(){
+   location.reload();
+}
+
+
+
+
+
+// const navy=document.getElementById('navy');
+// const gray=document.getElementById('gray');
+// const white=document.getElementById('dark');
+
+// navy.addEventListener('click',e=>{
+//     document.body.classList.add('--navy')
+
+//     document.body.classList.remove('')
+//     document.body.classList.remove('dark-theme')
+// });
+
+// white.addEventListener('click',e=>{
+//     document.body.classList.add('gray-theme')
+
+//     document.body.classList.remove('red-theme')
+//     document.body.classList.remove('dark-theme')
+// });
+
+// gray.addEventListener('click',e=>{
+//     document.body.classList.add('dark-theme')
+
+//     document.body.classList.remove('gray-theme')
+//     document.body.classList.remove('red-theme')
+// });
+
+
+
+// // navy .addEventListener('click',function(){
+// //     document.querySelector('.body').classList.remove('gray');
+// //     document.querySelector('.body').classList.remove('black');
+// //     document.querySelector('.body').classList.add('navy');
+// // });
+
+
+
